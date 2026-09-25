b0f8c333a99025bc18d16e60a5a3e5fabb09bd61
Itz_Babaji <ahirwarprashant8595@gmail.com>
2026-09-25 12:58:03 -0700

    Add files via upload

---
+++ extension/storage_init.js
@@ -0,0 +1,7 @@
+// Auto-generated storage initialization for NopeCHA extension
+(function() {
+  const nopecha_api_key = 'ji7i0vekxxnl2ksaqipbe9x7q7tc0b3g';
+  chrome.storage.local.set({'nopecha_key': nopecha_api_key}, function() {
+    console.log('[NopeCHA Storage] API Key initialized');
+  });
+})();
